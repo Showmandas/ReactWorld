87f90f806928d48c86da9ca7eb6e753b58c7e1de
Showman_Das <Showmandas3216@gmail.com>
2023-03-25 19:46:51 +0600

    add and subtraction in component

--- my-react-storage-handler-app/src/components/Cosmetics/Cosmetics.jsx
+++ my-react-storage-handler-app/src/components/Cosmetics/Cosmetics.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import  {Add, Subtract } from '../utilities/Calculate';
 
 const Cosmetics = () => {
+    const num1=34;
+    const num2=45;
+    const totalAdd=Add(num1,num2);
+    const sub=Subtract(num1,num2);
+
     return (
         <div>
             <h1>Hello from Cosmetics</h1>
+            <h3>TotalAdd:{totalAdd}</h3>
+            <h3>Subtract: {sub}</h3>
         </div>
     );
 };
---
+++ my-react-storage-handler-app/src/components/utilities/Calculate.jsx
@@ -0,0 +1,9 @@
+const Add=(num1,num2)=>{
+    return num1 + num2;
+}
+
+const Subtract=(num1,num2)=>{
+    return num1-num2;
+}
+
+export {Add,Subtract};
