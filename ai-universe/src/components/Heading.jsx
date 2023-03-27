65df0f87d06287dde307fa27f63167ac1d0e31b7
Showman_Das <Showmandas3216@gmail.com>
2023-03-27 07:57:33 +0600

    heading part complete

---
+++ ai-universe/src/components/Heading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Heading = () => {
+    return (
+        <div>
+            <h1 className='text-3xl font-bold'>AI Universe Hub</h1>
+        </div>
+    );
+};
+
+export default Heading;
