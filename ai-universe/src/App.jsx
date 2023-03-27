fa110eae21038fa4724d3ad588aec6d68fc69564
Showman_Das <Showmandas3216@gmail.com>
2023-03-27 07:51:54 +0600

    daisyui installation done

--- ai-universe/src/App.jsx
+++ ai-universe/src/App.jsx
@@ -8,26 +8,16 @@ function App() {
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <div className="card card-compact w-96 bg-base-100 shadow-xl">
+  <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
     </div>
   )
 }
--- ai-universe/tailwind.config.cjs
+++ ai-universe/tailwind.config.cjs
@@ -4,8 +4,31 @@ module.exports = {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+        
+"primary": "#e84efc",
+        
+"secondary": "#d0d863",
+        
+"accent": "#215996",
+        
+"neutral": "#141320",
+        
+"base-100": "#E8E5F0",
+        
+"info": "#5375D5",
+        
+"success": "#4FE8BA",
+        
+"warning": "#F4BC4E",
+        
+"error": "#E44956",
+        },
+      },
+    ],
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 }
