a632d6f706e88323bddde40d013dec2b380a1f76
Showman_Das <Showmandas3216@gmail.com>
2023-03-27 08:02:06 +0600

    sort by date button complete

---
+++ ai-universe/src/components/SortButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SortButton = () => {
+    return (
+        <div className='my-8'>
+            <button className="btn btn-error capitalize font-bold">Sort by Date</button>
+        </div>
+    );
+};
+
+export default SortButton;
