77e7ee681f9cf55f49d9865f3769eabf2aefa25e
Showman_Das <Showmandas3216@gmail.com>
2023-03-29 01:00:19 +0600

    get quantity of the product

--- ema-john-react/src/components/ShopCart/Cards/Cards.jsx
+++ ema-john-react/src/components/ShopCart/Cards/Cards.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Cart from '../../Cart/Cart';
 import Card from './Card';
 import './Card.css';
+import {addToDb, getShoppingCart} from '../../../utilities/fakedb'
 
 const Cards = () => {
     const[products,setProducts]=useState([])
@@ -11,9 +12,21 @@ const Cards = () => {
         .then(res=>res.json())
         .then(data=>setProducts(data))
     },[])
+    useEffect(()=>{
+        const storedCart=getShoppingCart()
+        // step-1 get id 
+        for(const id in storedCart){
+            // step 2-get the product by using id 
+            const savedProduct=products.find(product=>product.id === id)
+            // step-3: get quantity of the product 
+            const quantity=storedCart[id];
+            addedProduct.quantity=quantity
+        }
+    },[])
     const handleCart=(product)=>{
         const newCart=[...cart,product]
         setCart(newCart);
+        addToDb(product.id);
     }
 
     return (
