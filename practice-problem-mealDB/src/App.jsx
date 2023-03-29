88259840950d618dc090286c9443e58a14e87c97
Showman_Das <Showmandas3216@gmail.com>
2023-03-29 13:35:49 +0600

    fetch mealdb api

--- practice-problem-mealDB/src/App.jsx
+++ practice-problem-mealDB/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header/Header'
-import Meals from './components/Meals/MealCard/Meals'
+import Meals from './components/Meals/Meals/Meals'
 
 function App() {
   const [count, setCount] = useState(0)
