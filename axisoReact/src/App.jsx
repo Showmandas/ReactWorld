8e6bd6a6df548884d268cc680fc76c9d30ff1317
Showman_Das <Showmandas3216@gmail.com>
2023-04-03 18:34:33 +0600

    react router code

--- axisoReact/src/App.jsx
+++ axisoReact/src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [posts, setPosts] = useState([])
+  const [error, setError] = useState("")
 
+  useEffect(()=>{
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+    .then(res=>setPosts(res.data))
+    .catch(error=>setError(error))
+  },[])
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <>
+    <h2>Axios in react</h2>
+    {error !== "" && <h2>{error.message}</h2>}
+    <div className='grid'>
+    {
+      posts.map(post=>{
+        const{id,title,body}=post;
+        return <div className='card' key={id}>
+          <h2>{title.slice(0,15).toUpperCase()}</h2>
+          <p>{body.slice(0,100)}</p>
+        </div>
+      })
+    }
     </div>
+    </>
   )
 }
 
-export default App
+export default App;
