0b50cfc644ac9184cb4882f50ee7501cc8d6a2d0
Showman_Das <Showmandas3216@gmail.com>
2023-04-03 22:22:18 +0600

    fetch api data through loader function

---
+++ react-router/src/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div style={{display:"flex",justifyContent:"center",gap:"10px"}}>
+            <Link to="/">Home</Link>
+            <Link to="/about">about</Link>
+            <Link to="/friends">Friends</Link>
+            <Link to="/contact">contact</Link>
+        </div>
+    );
+};
+
+export default Header;
--- react-router/src/main.jsx
+++ react-router/src/main.jsx
@@ -9,22 +9,49 @@ import{
 import About from './Components/About/About';
 import Contact from './Components/Contact/Contact';
 import ErrorPage from './Components/ErrorPage/ErrorPage';
+import Home from './Components/Home/Home';
+import Friends from './Components/Friends/Friends';
+// import  { loader } from "./routes/root";
+
+
+// const router=createBrowserRouter([
+//   {
+//     path:"/",
+//     element:<div>This is Home app</div>
+//   },
+//   {
+//     path:"/about",
+//     element:<About/>,
+//     errorElement:<ErrorPage/>
+//   },
+//   {
+//     path:"/contact",
+//     element:<Contact/>,
+//     errorElement:<ErrorPage/>
+
+//   },
+// ])
+
 
 const router=createBrowserRouter([
   {
-    path:"/",
-    element:<div>This is Home app</div>
-  },
-  {
-    path:"/about",
-    element:<About/>,
-    errorElement:<ErrorPage/>
-  },
-  {
-    path:"/contact",
-    element:<Contact/>,
-    errorElement:<ErrorPage/>
-
+    path:'/',
+    element:<Home/>,
+    children:[
+      {
+        path:'about',
+        element:<About />
+      },
+      {
+        path:'contact',
+        element:<Contact />
+      },
+      {
+        path:'friends',
+        element:<Friends />,
+        loader:()=>fetch('https://jsonplaceholder.typicode.com/users')        
+      },
+    ]
   },
 ])
 
