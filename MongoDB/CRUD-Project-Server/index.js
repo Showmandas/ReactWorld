acbf768a01f185d3f28ac662f45da63d0d6f9d8e
Showman_Das <Showmandas3216@gmail.com>
2023-05-08 22:24:30 +0600

    delete done

--- MongoDB/CRUD-Project-Server/index.js
+++ MongoDB/CRUD-Project-Server/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors=require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
 const app=express()
 const port=process.env.PORT || 5000
 
@@ -35,6 +35,14 @@ async function run() {
         console.log(user)
         const result=await usersCollection.insertOne(user);
         res.send(result);
+    })
+
+    // delete user 
+    app.delete('/users/:id',async(req,res)=>{
+        const id=req.params.id;
+        const query={_id: new ObjectId(id)}
+        const result=await usersCollection.deleteOne(query)
+        res.send(result)
     })
     // await client.connect();
     // Send a ping to confirm a successful connection
--- MongoDB/CRUD-Project/mongoCrud/src/components/UserCard.jsx
+++ MongoDB/CRUD-Project/mongoCrud/src/components/UserCard.jsx
@@ -1,16 +1,34 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function UserCard({user}) {
-    const {name,email,phone}=user;
+    const[userdata,setUserData]=useState(user);
+    const {name,email,phone,_id}=userdata;
+    const handleDelete=(_id)=>{
+        console.log(_id);
+        fetch(`http://localhost:5000/users/${_id}`,{
+          method:'DELETE'
+        })
+        .then(res=>res.json())
+        .then(data=>{console.log(data)
+        if(data.deletedCount>0){
+          alert('delete successful')
+          const remaining=userdata.filter(users=>users._id !== _id)
+          setUserData(remaining);
+
+        }
+      })
+
+
+    }
   return (
     <div className='card mb-4 p-5 text-center'>
     <h2>{name}</h2>
     <p>{email}</p>
     <p>{phone}</p>
     <div className='d-flex justify-content-center gap-3 mt-4'>
-        <button className='btn bg-danger-subtle'>Delete</button>
+        <button className='btn bg-danger-subtle' onClick={()=>handleDelete(_id)}>Delete</button>
         <button className='btn bg-warning-subtle'>Update</button>
     </div>
     </div>
